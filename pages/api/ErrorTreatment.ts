f6c446750e97072b5620cc10131fbf3b77e9bd5a
Jhon171819 <jonathancosta1717@gmail.com>
2023-10-04 14:39:40 -0300

    Test_commit

--- pages/api/ErrorTreatment.ts
+++ pages/api/ErrorTreatment.ts
@@ -4,27 +4,27 @@ function handlerNotANumber (res: NextApiResponse): void {
   res.status(400).json({ message: 'Is not a number' })
 }
 
-function handlerBadRequest(res: NextApiResponse): void {
+function handlerBadRequest (res: NextApiResponse): void {
   res.status(400).json({ message: 'Bad request' })
 }
 
-function handlerNotFound(res: NextApiResponse): void {
+function handlerNotFound (res: NextApiResponse): void {
   res.status(404).json({ message: 'Not found' })
 }
 
-function handlerInvReqMethod(res: NextApiResponse): void {
+function handlerInvReqMethod (res: NextApiResponse): void {
   res.status(405).json({ message: 'Invalid request method' })
 }
 
-function handlerPTL(res: NextApiResponse): void {
+function handlerPTL (res: NextApiResponse): void {
   res.status(413).json({ message: 'Payload too large' })
 }
 
-function handlerIFRL(res: NextApiResponse): void {
+function handlerIFRL (res: NextApiResponse): void {
   res.status(429).json({ message: 'Internal fuction rate limit' })
 }
 
-function handlerIServerE(res: NextApiResponse): void {
+function handlerIServerE (res: NextApiResponse): void {
   res.status(500).json({ message: 'Internal server error' })
 }
 
--- pages/api/Operations.ts
+++ pages/api/Operations.ts
@@ -4,18 +4,18 @@ function add (a: number, b: number): number {
 }
 
 function sub (value1: number, value2: number): number {
-  const result = value1 - value2;
-  return result;
+  const result = value1 - value2
+  return result
 }
 
 function mult (value1: number, value2: number): number {
-  const result = value1 * value2;
-  return result;
+  const result = value1 * value2
+  return result
 }
 
 function div (value1: number, value2: number): number {
-  const result = value1 / value2;
-  return result;
+  const result = value1 / value2
+  return result
 }
 
 module.exports = {
@@ -23,4 +23,4 @@ module.exports = {
   sub,
   mult,
   div
-};
+}
